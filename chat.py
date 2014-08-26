6f95b71a80cbbfc6b4d28437e752bd49d8caf575
TIMTOWTDAT <xxlaguna93@gmail.com>
2014-08-27 01:32:58 +0800

    commit

--- chat.py
+++ chat.py
@@ -26,7 +26,7 @@ import os.path
 import jinja2
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)+"/html"),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
@@ -34,13 +34,14 @@ class ChatMessage(ndb.Model):
     user = ndb.StringProperty(required = True)
     timestamp = ndb.DateTimeProperty(auto_now_add=True)
     message = ndb.TextProperty(required = True)
+    chat = ndb.StringProperty(required = True)
     def __str__(self):
     	return "%s (%s): %s" % (self.user, self.timestamp, self.message)
 
+CHATS = ['main', 'book', 'flame']
 
 
-
-class ChatRoomPage(webapp2.RequestHandler):
+class ChatRoomLandingPage(webapp2.RequestHandler):
     def get(self):
 	user = users.get_current_user()
         if user is None:
@@ -49,15 +50,67 @@ class ChatRoomPage(webapp2.RequestHandler):
         else:
             
 	    self.response.headers['Content-Type'] = 'text/html'
-	    messages = ndb.gql("SELECT * FROM ChatMessage ORDER BY timestamp")
+	    messages = ndb.gql("SELECT * FROM ChatMessage ORDER BY timestamp " 					"DESC LIMIT 20")
+	    msg_list = messages.fetch()
+	    for msg in msg_list:
+		msg.deltatime = datetime.datetime.now() - msg.timestamp
 	    current_time = datetime.datetime.now() 
             template_values = {
 		'title': "TMTC's AppEngine Chat Room",
-		'msg_list': messages,
-		'current_time': current_time 
+		'msg_list': messages.fetch(),
+		'current_time': current_time,
+		'chats': CHATS
 		}
-	    template = JINJA_ENVIRONMENT.get_template('chat-template.html')
+	    template = JINJA_ENVIRONMENT.get_template('landing.html')
 	    page = template.render(template_values)
+	    self.response.out.write(page)
+    def post(self):
+	user = users.get_current_user()
+	if user is None:
+	    self.redirect(users.create_login_url(self.request.uri))
+	msgtext = self.request.get("message")
+	
+	if user.nickname() is None or user.nickname() == "":
+	    nick = "Anonymous"
+	else:
+	    nick = user.nickname()
+	msg = ChatMessage(user=nick,
+			  message=msgtext,
+			  chat="chat")
+	msg.put()
+	sys.stderr.write("****** Just stored message: %s" % msg)
+	# Now that we've added the message to the chat, we'll redirect 
+	# to the root page, which will make the user's browser refresh to 
+	# show the chat including their new message.
+	self.redirect('/')
+
+class GenericChatPage(webapp2.RequestHandler):
+    def get(self):
+	requested_chat = self.request.get("chat", default_value=None)
+	current_time = datetime.datetime.now()
+	if requested_chat == None or requested_chat not in CHATS:
+	    template_params = {
+		'title': "Error! Requested chat not found!",
+		'chatname': requested_chat,
+		'chats': CHATS,
+		'current_time': current_time
+		}
+	    self.redirect('../html/error.html')
+	else:
+	    messages = ndb.gql("SELECT * FROM ChatMessage WHERE chat = :1 " 
+			       "ORDER BY timestamp", requested_chat)
+	    for msg in messages:
+		msg.deltatime = datetime.datetime.now() - msg.timestamp
+	    current_time = datetime.datetime.now() 
+	    template_params = {
+	    	'title': "TMTC's AppEngine Chat Room",
+		'msg_list': messages,
+		'chat': requested_chat,
+		'chats': CHATS,
+		'current_time': current_time
+		}
+	    template = JINJA_ENVIRONMENT.get_template('multichat.html')
+	    page = template.render(template_params)
 	    self.response.out.write(page)
     def post(self):
 	user = users.get_current_user()
@@ -68,13 +121,23 @@ class ChatRoomPage(webapp2.RequestHandler):
 	    nick = "Anonymous"
 	else:
 	    nick = user.nickname()
+	chat = self.request.get("chat")	
 	msg = ChatMessage(user=nick,
-			  message=msgtext)
+			  message=msgtext,
+			  chat=chat)
 	msg.put()
 	sys.stderr.write("****** Just stored message: %s" % msg)
 	# Now that we've added the message to the chat, we'll redirect 
 	# to the root page, which will make the user's browser refresh to 
 	# show the chat including their new message.
-	self.redirect('/')
+	url = self.request.url
+	self.redirect(url)
 
-app = webapp2.WSGIApplication([('/', ChatRoomPage)])
+class ChatRoomPoster(webapp2.RequestHandler):
+    def get(self):
+	self.redirect('../html/poster.html')
+
+
+app = webapp2.WSGIApplication([('/', ChatRoomLandingPage),
+				('/talk', ChatRoomPoster),
+				('/enterchat', GenericChatPage)])
